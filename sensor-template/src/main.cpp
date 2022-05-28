3e025cd44da1079ef0de3a5a916161fae04e83be
Dan Borlovan <danb1974@gmail.com>
2022-05-28 19:46:29 +0300

    Document sensor init and blink led on radio error

--- sensor-template/src/main.cpp
+++ sensor-template/src/main.cpp
@@ -1,22 +1,43 @@
 #include <Arduino.h>
 #include <sensor.h>
 
-#define LED 9
+#define PIN_LED 9
 
 Sensor sensor;
 
+void flashLed(uint8_t count)
+{
+    for (uint8_t i = 0; i < count; i++) {
+        digitalWrite(PIN_LED, HIGH);
+        delay(100);
+        digitalWrite(PIN_LED, LOW);
+        delay(100);
+    }
+}
+
 void onDataReceived(const uint8_t *data, uint8_t size, uint8_t rssi)
 {
-    digitalWrite(LED, HIGH);
-    digitalWrite(LED, LOW);
+    digitalWrite(PIN_LED, HIGH);
+    digitalWrite(PIN_LED, LOW);
 }
 
 void setup()
 {
+    pinMode(PIN_LED, OUTPUT);
+
     const uint8_t key[16] = {0xd9, 0xc1, 0xbd, 0x60, 0x9c, 0x35, 0x3e, 0x8b, 0xab, 0xbc, 0x8d, 0x35, 0xc7, 0x04, 0x74, 0xef};
-    sensor.init(2, 1, key);
+
+    // on first programming of a board with flash, or when you want to change the radio setup
+    // example for high power radio and flash present
+    // sensor.init(99, 1, key, true, true);
+
+    // if flash present and config already saved
+    // sensor.init();
+
+    // no flash present, low power radio
+    sensor.init(99, 1, key);
+
     sensor.onMessage(onDataReceived);
-    pinMode(LED, OUTPUT);
 }
 
 void loop()
@@ -27,9 +48,7 @@ void loop()
         nextSend = millis() + 2000;
         if (sensor.sendAndWait((uint8_t *)"Hello 4", 7))
         {
-            digitalWrite(LED, HIGH);
-            delay(50);
-            digitalWrite(LED, LOW);
+            flashLed(10);
         }
     }
 
