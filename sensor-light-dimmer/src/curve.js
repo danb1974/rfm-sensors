17c0c4ced56510418957fdf17ffbebd785b422cc
Dan Borlovan <danb1974@users.noreply.github.com>
2024-02-16 19:10:48 +0200

    Improve light curve generation script

--- sensor-light-dimmer/src/curve.js
+++ sensor-light-dimmer/src/curve.js
@@ -1,8 +1,8 @@
 const GAMMA = 2;
 
-const MIN = 7.5e-3; //sec
-const MAX = 2.9e-3; //sec
-const TIMER = 2e6; //Hz
+const MIN = 7.0e-3; //sec
+const MAX = 3.0e-3; //sec
+const TIMER = 2e6;  //Hz
 
 const brightness = new Array(100).fill(0).map((_, i) => i + 1);
 const ticksMin = TIMER * MIN;
@@ -15,4 +15,12 @@ const ticksArray = brightness.map(b => {
     return Math.round(ticks);
 });
 
-console.log(ticksArray.join(','));
+console.log("// from", MIN * 1000, "to", MAX * 1000);
+console.log("static const uint16_t powerToTicks[100] = {");
+for (let i = 0; i < 10; i++) {
+    console.log("    "
+         + ticksArray.slice(i * 10, i * 10 + 10).map(n => ('     ' + n).slice(-5)).join(', ')
+         + ','
+    );
+}
+console.log("};");
